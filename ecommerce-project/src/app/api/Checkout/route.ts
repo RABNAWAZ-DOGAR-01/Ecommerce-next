cd8f8edba213bc845b7ae161bda649d6b2d7e227
RABNAWAZ-DOGAR-01 <rabnawazdogar9@gmail.com>
2025-02-07 17:56:05 +0500

    Update route.ts

--- ecommerce-project/src/app/api/Checkout/route.ts
+++ ecommerce-project/src/app/api/Checkout/route.ts
@@ -1,5 +1,5 @@
 
-mport { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
